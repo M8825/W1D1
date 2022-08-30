cb5e73fe165c7ce4d84cd9cc7224756b6441573a
Mlkz <maho.mamulashvili@gmail.com>
2022-08-30 11:17:03 -0400

    conditionals muscle memory

---
+++ expression_evaluation_round_2/conditionals_muscle_memory.js
@@ -0,0 +1,24 @@
+let num = 100;
+
+if (num > 5) {
+  console.log('if');
+}
+
+
+let num2 = 5;
+
+if (num2 > 5) {
+  console.log('if');
+} else {
+  console.log('else');
+}
+
+let num3 = 3;
+
+if (num3 < 0) {
+  console.log('if');
+} else if (num > 0 ) {
+  console.log('else if')
+} else {
+  console.log('else')
+}
