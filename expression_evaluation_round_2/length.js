1d4eb66f9cfc7bebddc65e74cf76185a4c2082e7
Mlkz <maho.mamulashvili@gmail.com>
2022-08-30 01:00:31 -0400

    length

---
+++ expression_evaluation_round_2/length.js
@@ -0,0 +1,9 @@
+let str1 = 'market'
+let num = 'whiteboard'.length - str1.length
+console.log(num); // 4
+
+let str2 = 'bootcamp';
+console.log(str2[num].toUpperCase()); // 'c'
+
+let char = str2[num].toLowerCase();
+console.log(char + '!'); // 'c!'
