9060f814890963f4aef2d1e6e83f8bd095b64c74
Mlkz <maho.mamulashvili@gmail.com>
2022-08-30 10:26:12 -0400

    evaluation and expressions

--- expression_evaluation_round_2/length.js
+++ expression_evaluation_round_2/length.js
@@ -7,3 +7,9 @@ console.log(str2[num].toUpperCase()); // 'c'
 
 let char = str2[num].toLowerCase();
 console.log(char + '!'); // 'c!'
+
+
+let sentence = 'welcome to bootcamp prep';
+let lastChar = sentence[sentence.length - 1];
+console.log(lastChar); // p
+console.log(sentence.indexOf(lastChar)); // the last element
