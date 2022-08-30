f648d2f9ce26977576b3b5b438636b139c5b6bb7
MkMula <mk@MkMulas-MBP.lan>
2022-08-30 00:53:46 -0400

    isEven and .indexOf

---
+++ expression_evaluation_round_2/even.js
@@ -0,0 +1,4 @@
+let num = 33;
+let isEvan = num % 2 === 0;
+console.log(isEvan);
+console.log(!isEvan);
---
+++ expression_evaluation_round_2/indexOf.js
@@ -0,0 +1,6 @@
+let idx = 'abcde'.indexOf('D');
+
+idx = idx + 11;
+console.log(idx);
+idx * 2;
+console.log(idx);
